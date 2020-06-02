c6d1f12baf5efac1d3bcf0125e46300b5e2e91ed
Ben Agro <ben.agro@mail.utoronto.ca>
2020-06-02 18:12:57 -0400

    data collection

--- master.sh
+++ master.sh
@@ -15,6 +15,8 @@ roscore &
 
 until rostopic list; do sleep 0.5; done #wait until rosmaster has started 
 
+rosparam load src/jackal_velodyne/params/simulation_params.yaml
+rosparam load src/myhal_simulator/params/common_vehicle_params.yaml
 rosparam load src/myhal_simulator/params/animation_params.yaml
 rosparam load src/myhal_simulator/params/room_params.yaml
 rosparam load src/myhal_simulator/params/scenario_params.yaml
