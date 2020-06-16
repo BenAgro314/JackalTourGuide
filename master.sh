df71362dc5605f9b719ec6d7e1f2be2f74fed809
Ben Agro <ben.agro@mail.utoronto.ca>
2020-06-16 17:39:34 -0400

    added scenario params

--- master.sh
+++ master.sh
@@ -19,7 +19,7 @@ rosparam load src/jackal_velodyne/params/simulation_params.yaml
 rosparam load src/myhal_simulator/params/common_vehicle_params.yaml
 rosparam load src/myhal_simulator/params/animation_params.yaml
 rosparam load src/myhal_simulator/params/room_params_V2.yaml
-rosparam load src/myhal_simulator/params/scenario_params.yaml
+rosparam load src/myhal_simulator/params/scenario_params_V2.yaml
 rosparam load src/myhal_simulator/params/plugin_params.yaml
 rosparam load src/myhal_simulator/params/model_params.yaml
 rosparam set use_sim_time true
--- myhal_test.sh
+++ myhal_test.sh
@@ -12,7 +12,7 @@ until rostopic list; do sleep 0.5; done #wait until rosmaster has started
 
 rosparam load src/myhal_simulator/params/animation_params.yaml
 rosparam load src/myhal_simulator/params/room_params_V2.yaml
-rosparam load src/myhal_simulator/params/scenario_params.yaml
+rosparam load src/myhal_simulator/params/scenario_params_V2.yaml
 rosparam load src/myhal_simulator/params/plugin_params.yaml
 rosparam load src/myhal_simulator/params/model_params.yaml
 rosparam load src/myhal_simulator/params/common_vehicle_params.yaml
