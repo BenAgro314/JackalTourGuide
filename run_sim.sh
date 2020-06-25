6dd9b651fbd643f2dd5b192e44606aeed2341e3c
Ben Agro <ben.agro@mail.utoronto.ca>
2020-06-25 07:14:27 -0400

    param changes

--- run_sim.sh
+++ run_sim.sh
@@ -7,6 +7,26 @@ killall rviz
 killall roscore
 killall rosmaster
 
+GUI=false
+TOUR="A_tour"
+MESSAGE=""
+LOADWORLD=""
+FILTER=false
+MAPPING=false
+
+while getopts t:m:g:l:f:a: option
+do
+case "${option}"
+in
+t) TOUR=${OPTARG};; # What tour is being used 
+m) MESSAGE=${OPTARG};; # A message to add to the log file
+g) GUI=${OPTARG};; # do you want to use the gui
+l) LOADWORLD=${OPTARG};; # do you want to load a prexisting world or generate a new one
+f) FILTER=${OPTARG};; # pointcloud filtering?
+a) MAPPING=${OPTARG};; # use gmapping?
+esac
+done
+
 roscore &
 
 until rostopic list; do sleep 0.5; done #wait until rosmaster has started 
@@ -22,5 +42,5 @@ echo $2 >> "/home/$USER/Myhal_Simulation/simulated_runs/$t/logs-$t/log.txt"
  
 sleep 0.1
 
-roslaunch jackal_velodyne jackal_testing.launch &
+roslaunch jackal_velodyne jackal_testing.launch mapping:=$MAPPING filter:=$FILTER &
 rosbag record -O "/home/$USER/Myhal_Simulation/simulated_runs/$t/raw_data.bag" -a -x "/kinect_V2(.*)" # Limiting data to remain under rosbag buffer
