133c9cb79962213e7f3233ee1383e51ce0fe0ca4
Ben Agro <ben.agro@mail.utoronto.ca>
2020-07-06 20:44:57 -0400

    script update: :

--- pytest.sh
+++ pytest.sh
@@ -6,4 +6,25 @@ killall roscore
 killall rosmaster
 killall rviz
 
-roslaunch dummy_classifier test.launch
+LOADWORLD=""
+RATE=1
+
+while getopts l:r: option
+do
+case "${option}"
+in
+l) LOADWORLD=${OPTARG};; 
+r) RATE=${OPTARG};; 
+esac
+done
+
+BAGPATH="/home/$USER/Myhal_Simulation/simulated_runs/$LOADWORLD/raw_data.bag"
+
+roscore -p $ROSPORT&
+
+until rostopic list; do sleep 0.5; done #wait until rosmaster has started 
+
+rosparam set use_sim_time true
+
+rosbag play -q -l -r $RATE $BAGPATH & 
+rosrun dummy_classifier lidarlistener.py
