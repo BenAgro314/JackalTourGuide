ab81f23c4f1c860c7fb3cf501021f80dd4b228a1
Ben Agro <ben.agro@mail.utoronto.ca>
2020-07-06 20:38:18 -0400

    testing numpy

--- src/dummy_classifier/src/lidarlistener.py
+++ src/dummy_classifier/src/lidarlistener.py
@@ -2,13 +2,22 @@
 
 import rospy
 from std_msgs.msg import String
-import numpy as np
+import ros_numpy
 
 from sensor_msgs.msg import PointCloud2, PointField
+from sensor_msgs import point_cloud2
 
 
-def on_scan(scan):
-	print("here")
+def on_scan(cloud):
+	points = ros_numpy.numpify(cloud)
+	print(points.shape)
+	print(points['x'].shape)
+	# list = point_cloud2.read_points_list(cloud)
+	# for f in cloud.fields:
+	# 	print(f)
+		
+	# print()
+	# print(list[0])
 
 
 def lidarlistener():
