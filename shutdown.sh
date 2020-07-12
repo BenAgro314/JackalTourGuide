97d34e5991334d550ea691523218eb683cb92713
Ben Agro <ben.agro@mail.utoronto.ca>
2020-07-12 12:10:16 -0400

    edited shutdown file

--- shutdown.sh
+++ shutdown.sh
@@ -8,7 +8,7 @@ killall gzclient
 
 sleep 0.5
 echo "Running data_processing.py"
-rosrun myhal_simulator data_processing.py $TIME
+rosrun dashboard data_processing.py $TIME
 #rosrun myhal_simulator process_bag $TIME
 #echo "Running bag diagnostics"
 #rosrun myhal_simulator bag_diagnostics $TIME $FILTER
